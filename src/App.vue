0de7ad9197e5c17222c8a339369783d39d2f875c
Joe Ray Gregory <joe.gregory@coding-bootcamps.eu>
2023-03-31 11:40:35 +0200

    feat: Add router

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>Welcome to Your Vue.js App</h1>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+  <router-view />
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
