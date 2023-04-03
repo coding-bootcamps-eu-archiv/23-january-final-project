4992efebb19a9476345fae20d94505bdf3b27715
Katharina Ephan <ephankatharina@gmail.com>
2023-04-03 17:12:11 +0200

    basic router for easier work

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingPageView from "../views/LandingPageView.vue";
+import CVClassicView from "../views/wip/CVClassicView.vue";
 
 const routes = [
   {
     path: "/",
     name: "landingpage",
     component: LandingPageView,
+  },
+  {
+    path: "/cv-classic",
+    name: "cv classic",
+    component: CVClassicView,
   },
 ];
 
---
+++ src/views/wip/CVClassicView.vue
@@ -0,0 +1,20 @@
+<template>
+  <CVClassic />
+</template>
+
+<script>
+import CVClassic from "@/components/templates/CVClassic.vue";
+export default {
+  name: "CVClassicView",
+  components: {
+    CVClassic,
+  },
+  // your script code goes here
+};
+</script>
+
+<style>
+* {
+  font-size: 15px;
+}
+</style>
