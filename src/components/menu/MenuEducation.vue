6156a0693a80fbf580d922db5289b80123c4e8a0
Muriel Cishek <muriel_cibra@hotmail.com>
2023-04-10 22:57:42 +0200

    remove all cancel buttons

--- src/components/menu/MenuEducation.vue
+++ src/components/menu/MenuEducation.vue
@@ -2,19 +2,7 @@
   <div class="menu-contain">
     <div class="menu-wrapper">
       <div class="menu-form">
-        <h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="bi bi-x"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-            />
-          </svg>
-          <span>Ausbildung</span>
-        </h1>
+        <h1>Ausbildung</h1>
         <div class="border-around">
           <p class="menu-full-width title-inside">
             Eintrag
@@ -127,10 +115,7 @@ h1 {
   display: flex;
   color: white;
   padding-bottom: 20px;
-}
-h1 > span {
   align-items: center;
-  padding-left: 0.8rem;
   font-weight: 400;
   font-size: 1.2rem;
 }
