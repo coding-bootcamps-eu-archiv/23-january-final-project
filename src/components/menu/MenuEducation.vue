02186f59d917077795ce666a2f7b9abef0e92588
Muriel Cishek <muriel_cibra@hotmail.com>
2023-04-11 19:03:36 +0200

    Add functionality in menuExperience and language

--- src/components/menu/MenuEducation.vue
+++ src/components/menu/MenuEducation.vue
@@ -1,151 +1,85 @@
 <template>
   <div class="menu-contain">
     <div class="menu-wrapper">
-      <div class="scroll menu-form">
-        <h1>Ausbildung</h1>
-        <v-expansion-panels>
-          <v-expansion-panel
-            class="border-around"
-            v-for="(panel, index) in panels"
-            :key="index"
+      <div class="menu-form">
+        <h1>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="bi bi-x"
+            viewBox="0 0 16 16"
           >
-            <v-expansion-panel-header
-              @click="selectedPanel = selectedPanel === index ? null : index"
-            >
-              <p class="menu-full-width title-inside text-subtitle">
-                Eintrag<span>
-                  <svg
-                    @click="deleteEducation(panel)"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    class="bi bi-trash"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"
-                    />
-                    <path
-                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"
-                    />
-                  </svg>
-
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    class="bi bi-chevron-up"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
-                    />
-                  </svg>
-                </span>
-              </p>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content
-              v-if="selectedPanel === index"
-              class="menu-full-width"
-            >
-              <label for="">Abschluss</label>
-              <v-text-field
-                variant="filled"
-                v-model="panel.degree"
-                placeholder="Bachelor im Ingenieurwesen"
-                class="text-input"
-                dense
-              />
-              <v-row dense>
-                <v-col>
-                  <p class="">
-                    <label for="">Bildungseinrichtung</label>
-                    <v-text-field
-                      variant="filled"
-                      v-model="panel.schoolName"
-                      placeholder="Technischr Universität Berlin"
-                      class="text-input"
-                      dense
-                    />
-                  </p>
-                </v-col>
-                <v-col cols="4">
-                  <p class="">
-                    <label for="">Ort</label>
-
-                    <v-text-field
-                      variant="filled"
-                      v-model="panel.place"
-                      placeholder="Berlin"
-                      class="text-input"
-                      dense
-                    /></p></v-col
-              ></v-row>
-              <v-row dense>
-                <v-col>
-                  <p class="">
-                    <label for="">Jahr</label>
-                    <v-text-field
-                      variant="filled"
-                      v-model="panel.yearStart"
-                      placeholder="2023"
-                      class="text-input"
-                    /></p
-                ></v-col>
-                <v-col>
-                  <p class="">
-                    <label for="">Monat</label>
-                    <v-text-field
-                      variant="filled"
-                      v-model="panel.monthStart"
-                      type="text"
-                      placeholder="Januar"
-                      class="text-input"
-                    /></p
-                ></v-col>
-                <v-col>
-                  <p class="">
-                    <label for="">Jahr</label>
-                    <v-text-field
-                      variant="filled"
-                      v-model="panel.yearEnd"
-                      type="text"
-                      placeholder="2023"
-                      class="text-input"
-                    />
-                  </p>
-                </v-col>
-                <v-col>
-                  <p class="">
-                    <label for="">Monat</label>
-                    <v-text-field
-                      variant="filled"
-                      v-model="panel.monthEnd"
-                      type="text"
-                      placeholder="Juni"
-                      class="text-input"
-                    />
-                  </p>
-                </v-col>
-              </v-row>
-              <v-row no-gutters>
-                <v-col> </v-col>
-
-                <v-col cols="5">
-                  <p class="mb-4 menu-full-width">
-                    <label class="menu-checkbox">
-                      <input type="checkbox" placeholder="" />
-                      <span class="menu-today-checkbox">Aktuell</span></label
-                    >
-                  </p>
-                </v-col></v-row
+            <path
+              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+          <span>Ausbildung</span>
+        </h1>
+        <div class="border-around">
+          <p class="menu-full-width title-inside">
+            Eintrag
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="bi bi-trash"
+                viewBox="0 0 16 16"
               >
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
+                <path
+                  d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"
+                />
+                <path
+                  d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"
+                />
+              </svg>
+            </span>
+          </p>
+          <p class="menu-full-width">
+            <label for="">Abschluss</label>
+            <input type="text" placeholder="Bachelor im Ingenieurwesen" />
+          </p>
+          <div class="menu-time">
+            <p class="">
+              <label for="">Bildungseinrichtung</label>
+              <input type="email" placeholder="Technische Universität Berlin" />
+            </p>
 
+            <p class="">
+              <label for="">Ort</label>
+              <input type="text" placeholder="Berlin" />
+            </p>
+          </div>
+          <div class="menu-time">
+            <p class="">
+              <label for="">Jahr</label>
+              <input type="text" placeholder="2020" />
+            </p>
+
+            <p class="">
+              <label for="">Monat</label>
+              <input type="text" placeholder="Januar" />
+            </p>
+
+            <p class="">
+              <label for="">Jahr</label>
+              <input type="text" placeholder="2023" />
+            </p>
+
+            <p class="">
+              <label for="">Monat</label>
+              <input type="text" placeholder="Juni" />
+            </p>
+          </div>
+          <p class="menu-full-width">
+            <label class="menu-checkbox">
+              <input type="checkbox" placeholder="" />
+              <span class="menu-today-checkbox">Aktuell</span></label
+            >
+          </p>
+        </div>
         <div class="menu-add-task-wrapper">
           <div class="menu-add-task">
-            <button class="menu-add-new" @click="addNewEducation">
+            <button class="menu-add-new">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="12"
@@ -181,110 +115,53 @@
   </div>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      panels: [
-        {
-          id: +new Date(),
-          job: "",
-          company: "",
-          place: "",
-          yearStart: "",
-          monthStart: "",
-          yearEnd: "",
-          monthEnd: "",
-        },
-      ],
-      selectedPanel: null,
-    };
-  },
-  methods: {
-    addNewEducation() {
-      if (this.panels.length == 3) {
-        alert("This demo version only allows a max of 3 Education titles.");
-      } else {
-        this.panels.push({
-          id: +new Date(),
-          degree: "",
-          schoolName: "",
-          place: "",
-          yearStart: "",
-          monthStart: "",
-          yearEnd: "",
-          monthEnd: "",
-        });
-      }
-    },
-    deleteEducation(panel) {
-      this.panels = this.panels.filter((item) => item.id !== panel.id);
-    },
-  },
-};
+export default {};
 </script>
 <style scoped>
 * {
   font-family: "roboto";
   box-sizing: border-box;
 }
-
-.scroll {
-  height: 600px;
-  overflow-y: scroll;
-}
-::-webkit-scrollbar {
-  width: 10px;
-}
-
-::-webkit-scrollbar-track {
-  background: #090c0f;
-  padding: 8rem;
-  border-radius: 10px;
-}
-
-::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 10px;
-}
-
 h1 {
   display: flex;
   color: white;
   padding-bottom: 20px;
+}
+h1 > span {
   align-items: center;
+  padding-left: 0.8rem;
   font-weight: 400;
   font-size: 1.2rem;
 }
-
 .title-inside {
   display: flex;
   align-items: center;
-  color: #fff;
 }
-
 .title-inside > span {
   padding-left: 0.6rem;
 }
-
 label {
   color: white;
   font-weight: 400;
-  font-size: 0.8rem;
 }
-
+input:focus {
+  outline: 1px solid var(--bs-primary);
+}
 input,
 button {
   width: 100%;
   border: 1px solid var(--bs-log);
 }
-
 form {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
-  align-items: center; /* Align menu-form items vertically center */
+  align-items: center;
 }
-
+.menu-time {
+  display: flex;
+  grid-column: 1 / span 2;
+}
 .menu-checkbox {
   display: flex;
   align-items: center;
@@ -294,7 +171,6 @@ form {
   width: 1rem;
   margin-right: 0.5rem;
 }
-
 .menu-today-checkbox {
   font-size: 0.6rem;
 }
@@ -308,30 +184,30 @@ form {
 .menu-add-task {
   grid-area: 2 / 3;
 }
-
-.text-input::v-deep .v-input__control {
+form input {
+  background: #d9d9d9;
+  border: 2px solid var(--bs-log);
+  border-radius: 5px;
+  padding: 0.5rem; /* Add padding to inputs */
+}
+::placeholder {
+  color: var(--bs-log);
+  opacity: 0.8;
+}
+input {
   background: #d9d9d9;
   border: 2px solid var(--bs-log);
   border-radius: 5px;
 }
-.text-input::v-deep .v-input__control input::placeholder {
-  color: var(--bs-log);
-  opacity: 0.8;
-  font-size: 0.8rem;
-}
-
 p {
-  color: var(--bs-log);
-  font-size: 0.8rem;
+  color: #fff;
 }
-
 button,
 input,
 textarea {
   padding: 0.5em;
   font-size: 0.8rem;
 }
-
 .menu-next-button {
   font-size: 1rem;
   padding: 1rem 1rem;
@@ -357,7 +233,6 @@ textarea {
   cursor: pointer;
   transition: all 0.3s linear;
 }
-
 .menu-back-button:hover,
 .menu-back-button:focus {
   background: var(--bs-text-light);
@@ -372,17 +247,6 @@ textarea {
   border-radius: 10px;
   cursor: pointer;
   transition: all 0.3s linear;
-  margin-bottom: 22rem;
-}
-.bi-chevron-up {
-  position: relative;
-  left: 11rem;
-  float: right;
-  margin-right: 10px;
-  width: 20px;
-  height: 20px;
-}
-.v-expansion-panel {
-  background: none;
+  margin-bottom: 15rem;
 }
 </style>
