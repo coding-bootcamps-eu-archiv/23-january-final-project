b4cab89c3d2e395faa1962f802e5e912ea867530
Muriel Cishek <muriel_cibra@hotmail.com>
2023-04-12 10:51:47 +0200

    changes in cv generator

--- src/views/CVGeneratorView.vue
+++ src/views/CVGeneratorView.vue
@@ -231,22 +231,7 @@
     <section class="cv-layout">
       <div class="right">
         <div class="top no-print">
-          <h2>My Resume- Seite 1/2</h2>
-          <button>
-            <i class="fas fa-sign-out-print"
-              ><svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-bookmark"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
-                /></svg></i
-            >Speichern
-          </button>
+          <h2>My Resume</h2>
         </div>
         <div
           class="resume printable"
